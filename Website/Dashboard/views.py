74a552fb0e6dd9bec64c9346fdd008222262dbef
Muhd Mujib <muhdiqmal1997@gmail.com>
2020-02-07 19:54:33 +0800

    update

--- Website/Dashboard/views.py
+++ Website/Dashboard/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+def Home(request):
+    return render(request, 'Frontpage.html' )
--- Website/Website/urls.py
+++ Website/Website/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Dashboard import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('Home/', views.Home),
 ]

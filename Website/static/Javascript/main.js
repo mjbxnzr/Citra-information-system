d64c242ddb46cbe3fccd2dfb390fee86db80a764
Muhd Mujib <muhdiqmal1997@gmail.com>
2020-02-13 15:02:59 +0800

    added nav bar

--- Website/static/Javascript/main.js
+++ Website/static/Javascript/main.js
@@ -1,10 +1,16 @@
-var app = new Vue({
+
+var staff = new Vue({
     el: '#STAFF',
     data:{
-        STAFF_DESC: 'University Malaya Staff are able to view FAQ section and download the required document from CiTra'
-    }
-
+        STAFF_DESC: 'University Malaya Staff are able to view FAQ section and download the required document from CiTra',
+    },
+    delimiters: ["[[","]]"]
 })
 
-Vue.config.devtools = true;
-Vue.config.delimeters=["[[","]]"];
+var student = new Vue({
+    el: '#STUDENT',
+    data:{
+        STUDENT_DESC:'Student are able to check for class timetable that only related to CITRA subject and browse on the FAQ if you have any doubts in mind.'
+    },
+    delimiters: ["[[","]]"]
+})

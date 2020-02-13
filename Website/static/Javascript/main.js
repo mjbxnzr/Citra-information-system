22d4dc3cd8135a46861f19d6beebf83caa9731da
Muhd Mujib <muhdiqmal1997@gmail.com>
2020-02-13 13:27:21 +0800

    apply Vue framework

---
+++ Website/static/Javascript/main.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+    el: '#STAFF',
+    data:{
+        STAFF_DESC: 'University Malaya Staff are able to view FAQ section and download the required document from CiTra'
+    }
+
+})
